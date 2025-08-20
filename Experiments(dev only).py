d90bdaaf924643eec55ebc2a32f0d2c89ed3a636
Suparnajit <suparnajit.study@gmail.com>
2025-08-20 21:40:45 +0530

    Refining the conditions and omitting small errors

--- Experiments(dev only).py
+++ Experiments(dev only).py
@@ -12,7 +12,7 @@ def lorentz(t,y):
     x, y_pos, vx, vy = y
     r = np.sqrt(x ** 2 + y_pos ** 2)
 
-    if r<3 :
+    if r<0.04 :
         B = np.array([0, 0, 1])
         w = (q / m) * float(np.linalg.norm(B))
     else:
@@ -20,22 +20,22 @@ def lorentz(t,y):
         w = (q / m) * float(np.linalg.norm(B))
 
 
-    if -0.0025<x<0.0025 or -3<y_pos<3:
-        E = np.array([((10**6) * np.sin(t * w)), 0, 0])
+    if -0.0025<x<0.0025 and -0.04<y_pos<0.04:
+        E = np.array([((10**6) * np.sign(np.sin(t * w))), 0, 0])
     else:
         E=np.array([0,0,0])
 
     v=np.array([vx,vy,0])
     a=(q/m)*(E+np.cross(B,v))
-    print(y)
-    return [vx,vy,a[0],a[1]]
 
+    return [vx,vy,a[0],a[1]]
+print("Your simulation is running")
 y0=[0,0,0,0]
 
-t_sp=(0,10**-5)
+t_sp=(0,10**-6)
 t_eval=np.linspace(t_sp[0],t_sp[1],20000)
 
-sol=solve_ivp(lorentz,t_sp,y0,t_eval=t_eval,max_step=(10**-10))
+sol1=solve_ivp(lorentz,t_sp,y0,t_eval=t_eval,max_step=(10**-10))
 
 
 
@@ -43,9 +43,16 @@ sol=solve_ivp(lorentz,t_sp,y0,t_eval=t_eval,max_step=(10**-10))
 
 plt.figure(figsize=(5,5))
 
-plt.plot(sol.y[0], sol.y[1], linewidth=0.8)
+plt.plot(sol1.y[0], sol1.y[1],linewidth=0.8)
 plt.xlabel('x (m)'); plt.ylabel('y (m)')
 plt.title('Trajectory (x vs y)')
 plt.axis('equal')
+plt.grid(True)
+plt.show()
+
+k=0.5*m*((sol1.y[2]**2)+(sol1.y[3]**2))
+
+
+plt.plot(t_eval,k)
 plt.grid(True)
 plt.show()
